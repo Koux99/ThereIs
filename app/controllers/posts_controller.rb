d82dc9894fdd27a894314fa9e32a0f65b022a459
Koux99 <mk.smile.shot@gmail.com>
2021-09-10 20:34:39 +0900

    before_actionの追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,9 @@
 class PostsController < ApplicationController
+
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :admin_check, except: [:index, :show]
+  
   
   def index
     @posts = Post.all
@@ -18,15 +23,12 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       redirect_to post_path(@post.id)
     else
@@ -35,7 +37,6 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to root_path
   end
@@ -44,4 +45,15 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:title, :lead_text, :text, :image).merge(user_id: current_user.id)
   end
+  
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def admin_check
+    unless current_user.admin?
+      redirect_to root_path
+    end
+  end
+
 end
