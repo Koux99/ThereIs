b35463cccfc60d6ced2298e50ce65bfab82be179
Koux99 <mk.smile.shot@gmail.com>
2021-09-22 14:10:00 +0900

    Postモデル単体テストコード

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,3 +1,38 @@
 require 'rails_helper'
 
+RSpec.describe Post, type: :model do
+  before do
+    @post = FactoryBot.build(:post)
+  end
 
+  describe '記事投稿' do
+    context '記事を投稿できるとき' do
+      it '全ての入力が存在すれば登録できる' do
+        expect(@post).to be_valid
+      end
+    end
+    context '記事を投稿できないとき' do
+      it 'titleが空では投稿できない' do
+        @post.title = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Title can't be blank")
+      end
+      it 'lead_textが空では出品できない' do
+        @post.lead_text = ''
+        @post.valid?
+        binding.pry
+        expect(@post.errors.full_messages).to include("Lead text can't be blank")
+      end
+      it 'textが空では出品できない' do
+        @post.text = ''
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Text can't be blank")
+      end
+      it 'imageが空では出品できない' do
+        @post.image = nil
+        @post.valid?
+        expect(@post.errors.full_messages).to include("Image can't be blank")
+      end
+    end
+  end
+end
