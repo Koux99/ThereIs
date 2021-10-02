9b156d16344db4d22cc6b91178ba398128cd72b0
Koux99 <mk.smile.shot@gmail.com>
2021-10-02 17:48:27 +0900

    リポジトリ名修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.16.0"
 
 set :application, "ThereIs"
-set :repo_url, "git@example.com:Koux99/Thereis.git"
+set :repo_url, "git@github.com:Koux99/Thereis.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
